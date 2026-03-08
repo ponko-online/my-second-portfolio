03f66b328f46475f702efea27fc3bb6cb351c7bf
ぽんこ＠オンライン秘書 <hitujinoyagisan@gmail.com>
2026-03-08 16:58:36 +0900

    Add files via upload

---
+++ style.css
@@ -0,0 +1,17 @@
+function showPage1() {
+  document.getElementById("page1").style.display = "block";
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page3").style.display = "none";
+}
+
+function showPage2() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+  document.getElementById("page3").style.display = "none";
+}
+
+function showPage3() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page3").style.display = "block";
+}
